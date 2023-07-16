ff31345df7aaadba34aa1bd37a31d7dbcb699d59
Will <70348218+imaginary-png@users.noreply.github.com>
2023-07-16 12:11:47 +1000

    add: index and form views, new GET/POST router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+
+const router = express.Router();
+
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+    },
+    {
+        text: "Howdy!",
+        user: "Brown",
+        added: new Date()
+    },
+    {
+        text: "Hola!",
+        user: "Milly",
+        added: new Date()
+    }
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res, next) => {
+    res.render("index", { title: "Mini Messageboard", messages });
+});
+
+/* GET new message */
+router.get("/new", (req, res, next) => {
+    res.render("form", { title: "new message" });
+});
+
+/* POST new message */
+router.post("/new", (req, res, next) => {
+    const { name, msg } = req.body;
+    messages.push({ text: msg, user: name, added: new Date() });
+    res.redirect("/");
 });
 
 module.exports = router;
